1850676a79281cd9ed52853d494ddf8fe8e1c860
rose.crisp <rocrisp@redhat.com>
2021-03-29 10:52:26 -0400

    add the gut of the script

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
-#cat package.yaml | yq '.channels[] | {name} | .[]'
 export OO_INSTALL_NAMESPACE="!create"
-export OO_TARGET_NAMESPACES="!all"
 export OO_INDEX="registry.redhat.io/redhat/certified-operator-index:v4.7"
 export ARTIFACT_DIR="artifact_dir"
 export SHARED_DIR="shared_dir"
-i=0
+export OO_TARGET_NAMESPACES="!all"
+counter=0
 for file in $(find $PWD/$1 -name 'package.yaml'); 
 do 
 	echo $file;
 	export OO_PACKAGE=$(cat $file | yq '.packageName' | sed -e 's/^"//' -e 's/"$//');
-	echo $OO_PACKAGE;
+	echo $OO_PACKAGE
 	#key=$(echo $OO_PACKAGE | cut -d'-' -f1);
 	#echo 'operator name' $key
 	export OO_CHANNEL=$(cat $file | yq '.channels[] | {name} | .[]' | sed -e 's/^"//' -e 's/"$//');
-	echo $OO_CHANNEL;
+	echo $OO_CHANNEL
+        echo $OO_TARGET_NAMESPACES 
 	#currentCSV=$(cat $file | yq '.channels[] | {currentCSV} | .[]' | sed -e 's/^"//' -e 's/"$//');
 	#echo 'cvs' $currentCSV
 	#csvdir=$(echo $currentCSV | cut -d'.' -f2- | sed 's/v//');
@@ -32,21 +32,16 @@ do
 	#break
         #cat $csvfile | yq '.spec | {installModes} | .[] | .[] | {supported}'
 	#break	
-	./subscribe-command-v2.sh
-	
-	status=$?
-        echo "Output rc = " $status	
-	if [[ $status -ne 0 ]]; then
+	output=$(./subscribe-command-v2.sh)
+	if [[ $output == *"Timed out waiting for csv to become ready"* ]]; then
 		export OO_TARGET_NAMESPACES="!install"
 		echo "Run the command again with OO_TARGET_NAMESPACES = $OO_TARGET_NAMESPACES"
 		./subscribe-command-v2.sh
 	fi
-        
-	echo $'--------------------\n'
+        ((counter++))
+        echo "Installing number $counter $OO_PACKAGE" >> success.txt	
+	echo $'Installing Operator $counter\n'
+	echo $'----------------------------\n'
+	export OO_TARGET_NAMESPACES="!all"
 
-	((i++))
-	if [[ $i -eq 10 ]]; then
-		echo "Counter : " $i
-	        break
-	fi	
 done
