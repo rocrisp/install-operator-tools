fea2a40c86ee47e6e27aa2674a95c460faabfed4
rose.crisp <rocrisp@redhat.com>
2021-05-12 16:36:28 -0400

    put quotes around status

--- bin/transpose.sh
+++ bin/transpose.sh
@@ -6,14 +6,14 @@ inputfile=../operatorlist/$1
 #"operators": [
 
 while IFS= read -r line; do 
-    # echo $counter
-    # echo "text read from file: $line"
+    echo $counter
+    echo "text read from file: $line"
     operator_name=$(echo $line| cut -d':' -f1)
     operator_status=$(echo $line| cut -d':' -f2)
     operand_status=$(echo $line| cut -d':' -f3)
-    # echo "Operator Name is $operator_name"
-    # echo "Operator Status is $operator_status"
-    # echo "Operand Status is $operand_status"
+    echo "Operator Name is $operator_name"
+    echo "Operator Status is $operator_status"
+    echo "Operand Status is $operand_status"
 
 #     {
 #     id: $t1 ,  
@@ -30,8 +30,8 @@ while IFS= read -r line; do
         --arg t2 $operator_name \
         --arg t3 "starttime" \
         --arg t4 "endtime" \
-        --arg t5 $operator_status \
-        --arg t6 $operand_status \
+        --arg t5 "$operator_status" \
+        --arg t6 "$operand_status" \
         -f template.json >> $inputfile.temp.json
 
 ((counter++))
