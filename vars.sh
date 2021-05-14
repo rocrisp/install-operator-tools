429cc0fc98008b6470d645abfcef9c4cbd640dcd
John McKenzie <john@mkz.io>
2021-05-14 13:56:18 -0500

    Allow override of all vars

--- vars.sh
+++ vars.sh
@@ -4,9 +4,9 @@ export INSTALL_RETRY=${INSTALL_RETRY:-"yes"} # re-install operators which have f
 export INSTALL_NEWTEST=${INSTALL_NEWTEST:-"no"} # Run All test again
 
 # Don't need to change these vars
-export OO_INDEX="registry.redhat.io/redhat/certified-operator-index:v4.7"
-export ARTIFACT_DIR="artifact_dir"
-export SHARED_DIR="shared_dir"
-export INSTALL_CR_YML="crs/cr0.yml"
+export OO_INDEX=${OO_INDEX:-"registry.redhat.io/redhat/certified-operator-index:v4.7"}
+export ARTIFACT_DIR=${ARTIFACT_DIR:-"artifact_dir"}
+export SHARED_DIR=${SHARED_DIR:-"shared_dir"}
+export INSTALL_CR_YML=${INSTALL_CR_YML:-"crs/cr0.yml"}
 
-export INSTALL_SOURCEOFTRUTH="results.txt" #operatorlist
+export INSTALL_SOURCEOFTRUTH=${INSTALL_SOURCEOFTRUTH:-"results.txt"} #operatorlist
